b94ae9eba28fea08a0220194d039eff7af22c821
roeihers <rohersko@gmail.com>
2021-09-21 10:03:59 +0300

    First Try

--- Kitsune.py
+++ Kitsune.py
@@ -23,6 +23,7 @@ from KitNET.KitNET import KitNET
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+# MYCOMMENT
 class Kitsune:
     def __init__(self,file_path,limit,max_autoencoder_size=10,FM_grace_period=None,AD_grace_period=10000,learning_rate=0.1,hidden_ratio=0.75,):
         #init packet feature extractor (AfterImage)
@@ -32,7 +33,7 @@ class Kitsune:
         self.AnomDetector = KitNET(self.FE.get_num_features(),max_autoencoder_size,FM_grace_period,AD_grace_period,learning_rate,hidden_ratio)
 
     def proc_next_packet(self):
-        # create feature vector
+        # create feature vectors
         x = self.FE.get_next_vector()
         if len(x) == 0:
             return -1 #Error or no packets left
