1d014fa581dcce0aaa5bcc90d0a2219ae141d14c
roeihers <rohersko@gmail.com>
2021-09-21 10:20:06 +0300

    First Try

--- KitNET/OurProject.py
+++ KitNET/OurProject.py
@@ -16,3 +16,12 @@ class Encoder:
 
 
 
+class decoder:
+    def __init__(self,):
+        pass
+
+    def decode(self, input):
+        if hp["decoder"] == "Kitsune":
+            return sigmoid(numpy.dot(input, self.W) + self.hbias)
+        elif hp["decoder"] == "HerskoNet":
+            pass
