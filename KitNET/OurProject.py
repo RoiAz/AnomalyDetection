bfca830f47ea18fbdbd5f471caf7b1ff7a83e547
RoiAz <roiazarzar123@gmail.com>
2021-09-22 19:45:06 +0200

    Pytorch + medium pcap

--- KitNET/OurProject.py
+++ KitNET/OurProject.py
@@ -1,8 +1,54 @@
 from KitNET.expriement import hyperparms
 from KitNET.utils import *
+import torch
+import torch.nn as nn
 
 hp = hyperparms()
 rng = numpy.random.RandomState(1234)
+
+
+class EncoderCNN(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        modules = []
+        channel_list = [in_channels] + [128] + [256] + [512] + [out_channels]
+        for ci in range(1, len(channel_list)):
+            modules.append(
+                nn.Conv1d(in_channels=channel_list[ci - 1], out_channels=channel_list[ci], kernel_size=3, stride=2))
+            modules.append(nn.BatchNorm1d(channel_list[ci]))
+            modules.append(nn.LeakyReLU(negative_slope=0.05))
+        self.cnn = nn.Sequential(*modules)
+
+    def forward(self, x):
+        x = torch.tensor(x)
+        #x = x.view(-1)
+        return self.cnn(x)
+
+
+class DecoderCNN(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        modules = []
+        channel_list = [in_channels] + [512] + [256] + [128] + [out_channels]
+
+        for ci in range(1, len(channel_list)):
+            if ci == len(channel_list) - 1:
+                modules.append(
+                    nn.ConvTranspose1d(in_channels=channel_list[ci - 1], out_channels=channel_list[ci], kernel_size=3,
+                                       stride=2, output_padding=1))
+            else:
+                modules.append(
+                    nn.ConvTranspose1d(in_channels=channel_list[ci - 1], out_channels=channel_list[ci], kernel_size=3,
+                                       stride=2))
+            modules.append(nn.BatchNorm1d(channel_list[ci]))
+            modules.append(nn.LeakyReLU(negative_slope=0.05))
+        self.cnn = nn.Sequential(*modules)
+
+    def forward(self, h):
+        # Tanh to scale to [-1, 1] (same dynamic range as original images).
+        h = torch.tensor(h)
+        h = h.view(-1)
+        return torch.tanh(self.cnn(h))
 
 
 class Norm:
@@ -32,7 +78,10 @@ class autoEncoder:
         self.input = 0
         self.encode_output = 0
         self.decode_output = 0
-        if hp["encoder"] == "Kitsune":
+        self.encoder = None
+        self.decoder = None
+
+        if hp["net"] == "Kitsune":
             a = 1. / self.n_visible
             self.hbias = numpy.zeros(self.n_hidden)  # initialize h bias 0
             self.vbias = numpy.zeros(self.n_visible)  # initialize v bias 0
@@ -42,23 +91,30 @@ class autoEncoder:
                 size=(self.n_visible, self.n_hidden)))
             self.W_prime = self.W.T
 
+        if hp["net"] == "PNet":
+            self.in_channels = hp["in_channels"]
+            self.h_dim = hp['h_dim']
+            self.out_channels = hp["out_channels"]
+            self.encoder = EncoderCNN(in_channels=self.in_channels, out_channels=self.h_dim)
+            self.decoder = DecoderCNN(in_channels=self.h_dim, out_channels=self.out_channels)
+
     def encode(self, x):
         self.input = x
-        if hp["encoder"] == "Kitsune":
+        if hp["net"] == "Kitsune":
             self.encode_output = sigmoid(numpy.dot(x, self.W) + self.hbias)
             return self.encode_output
-        elif hp["encoder"] == "PNet":
-            pass
+        elif hp["net"] == "PNet":
+            return self.encoder.forward(x)
 
     def decode(self, x):
-        if hp["decoder"] == "Kitsune":
+        if hp["net"] == "Kitsune":
             self.decode_output = sigmoid(numpy.dot(x, self.W_prime) + self.vbias)
             return self.decode_output
-        elif hp["decoder"] == "PNet":
-            pass
+        elif hp["net"] == "PNet":
+            return self.decoder.forward(x)
 
     def train(self):
-        if hp["encoder"] == "Kitsune":
+        if hp["net"] == "Kitsune":
             self.L_h2 = self.input - self.decode_output
             L_h1 = numpy.dot(self.L_h2, self.W) * self.encode_output * (1 - self.encode_output)
             L_hbias = L_h1
--- KitNET/expriement.py
+++ KitNET/expriement.py
@@ -1,6 +1,6 @@
 def hyperparms():
     hp = dict(
-        encoder_lr=0.0005, decoder_lr=0.0005, encoder="Kitsune", decoder="Kitsune",
-        corruption_level=0.001, norm="Knorm", decode_train=True)
+        encoder_lr=0.0005, decoder_lr=0.0005, net="Kitsune",
+        corruption_level=0.001, norm="Knorm", decode_train=True, in_channels=1, out_channels=1, h_dim=1)
 
     return hp
